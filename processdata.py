3427eaf80429fd6ac6db5a52418524065717c8a2
Cindy Zhou <cindy_zhou@sjtu.edu.cn>
2020-06-12 13:38:39 +0800

    add ip merger table

--- processdata.py
+++ processdata.py
@@ -48,12 +48,23 @@ sql_backup = '''
 select g.payer_account, h.name as project_name, g.backup_name, g.backup_size_gb  From (select e.*, f.name as payer_account from (select c.* ,d.payer_id from (select a.*, b.value as backup_size_gb from ( select  cinder.backups.id as backup_id, cinder.backups.display_name as backup_name, cinder.backups.project_id from cinder.backups where cinder.backups.deleted=0 ) a left join (select backup_metadata.backup_id, backup_metadata.value  from cinder.backup_metadata where backup_metadata.deleted=0 and backup_metadata.key = 'size') b on a.backup_id = b.backup_id  ) c left join (select keystone.assignment.actor_id as payer_id ,keystone.assignment.target_id  from keystone.assignment where keystone.assignment.role_id = "22f8b7794e5f40f4a21b1c74087936ae") d on c.project_id = d.target_id) e left join (select keystone.local_user.user_id, keystone.local_user.name  FROM keystone.local_user) f on e.payer_id=f.user_id WHERE f.name REGEXP 'jcloud_' ) g left join keystone.project h on g.project_id=h.id ORDER BY h.name;
 '''
 
-#sql_newuser = '''
-#SELECT  * FROM keystone.local_user WHERE user_id IN (select id from keystone.user  WHERE json_value(extra,'$.consumption') > 0 and DATE_SUB(CURDATE(), INTERVAL 7 DAY) <= date(created_at));
-#'''
+sql_ip_null = '''
+SELECT g.payer_account, h.project_name, g.floating_ip_address, g.fixed_port_id as device_owner, g.fixed_port_id as device_name from (SELECT e.*,f.name AS payer_account FROM (SELECT a.project_id,  a.floating_ip_address,  a.fixed_port_id , b.payer_id FROM (SELECT neutron.floatingips.project_id, neutron.floatingips.floating_ip_address, neutron.floatingips.fixed_port_id FROM neutron.floatingips ) a LEFT JOIN (select keystone.assignment.actor_id as payer_id ,keystone.assignment.target_id  from keystone.assignment where keystone.assignment.role_id = "22f8b7794e5f40f4a21b1c74087936ae") b on a.project_id = b.target_id ) e left join (select keystone.local_user.user_id, keystone.local_user.name  FROM keystone.local_user) f on e.payer_id=f.user_id WHERE f.name REGEXP 'jcloud_' AND e.fixed_port_id IS NULL AND floating_ip_address REGEXP '111.')g  left JOIN (select keystone.project.name AS project_name, keystone.project.id FROM keystone.project) h on g.project_id=h.id
+'''
 
+sql_ip_vm = '''
+SELECT k.payer_account, k.project_name, k.floating_ip_address, k.device_owner, m.vm_name as device_name FROM (SELECT * FROM (SELECT g.payer_account, h.project_name, g.floating_ip_address, g.fixed_port_id from (SELECT e.*,f.name AS payer_account FROM (SELECT a.project_id,  a.floating_ip_address,  a.fixed_port_id, a.fixed_ip_address , b.payer_id FROM (SELECT neutron.floatingips.project_id, neutron. floatingips.floating_ip_address, neutron.floatingips.fixed_port_id, neutron.floatingips.fixed_ip_address FROM neutron.floatingips ) a LEFT JOIN (select keystone.assignment.actor_id as payer_id ,keystone.assignment.target_id  from keystone.assignment where keystone.assignment.role_id = "22f8b7794e5f40f4a21b1c74087936ae") b on a.project_id = b.target_id ) e left join (select keystone.local_user.user_id, keystone.local_user.name  FROM keystone.local_user) f on e.payer_id=f.user_id WHERE f.name REGEXP 'jcloud_' AND e.fixed_port_id IS not NULL  AND e.floating_ip_address REGEXP '111.' AND e.fixed_ip_address IS NOT NULL)g  left JOIN (select keystone.project.name AS project_name, keystone.project.id FROM keystone.project) h on g.project_id=h.id ) i LEFT JOIN (select neutron.ports.id, neutron.ports.device_id , neutron.ports.device_owner from neutron.ports  ) j ON i.fixed_port_id=j.id ) k LEFT JOIN (SELECT nova.instances.display_name AS vm_name, nova.instances.uuid FROM nova.instances WHERE nova.instances.deleted = 0) m ON k.device_id = m.uuid WHERE k.device_owner REGEXP 'compute:nova'
+'''
 
-# 连接设置 连接mysql 用户名ffzs 密码666 地址localhost：3306 database：stock
+sql_ip_router = '''
+SELECT m.payer_account, m.project_name, m.floating_ip_address, m.device_owner, n.name AS device_name FROM (SELECT * FROM (SELECT g.payer_account, h.project_name, g.floating_ip_address, g.fixed_port_id, g.fixed_ip_address from (SELECT e.*,f.name AS payer_account FROM (SELECT a.project_id,  a.floating_ip_address,  a.fixed_port_id, a.fixed_ip_address , b.payer_id FROM (SELECT neutron.floatingips.project_id, neutron.floatingips.floating_ip_address, neutron.floatingips.fixed_port_id, neutron.floatingips.fixed_ip_address FROM neutron.floatingips ) a LEFT JOIN (select keystone.assignment.actor_id as payer_id ,keystone.assignment.target_id  from keystone.assignment where keystone.assignment.role_id = "22f8b7794e5f40f4a21b1c74087936ae") b on a.project_id = b.target_id ) e left join (select keystone.local_user.user_id, keystone.local_user.name  FROM keystone.local_user) f on e.payer_id=f.user_id WHERE f.name REGEXP 'jcloud_' AND e.floating_ip_address REGEXP '111.' AND e.fixed_port_id IS NOT NULL AND e.fixed_ip_address IS null )g  left JOIN (select keystone.project.name AS project_name, keystone.project.id FROM keystone.project) h on g.project_id=h.id ) i LEFT JOIN (select neutron.ports.id, neutron.ports.device_id , neutron.ports.device_owner from neutron.ports  ) j ON i.fixed_port_id=j.id)m LEFT JOIN (SELECT neutron.routers.name, neutron.routers.id FROM neutron.routers) n ON m.device_id = n.id
+'''
+
+sql_ip_database = '''
+SELECT k.payer_account, k.project_name, k.floating_ip_address, k.device_owner, m.database_name as device_name FROM (SELECT * FROM (SELECT g.payer_account, h.project_name, g.floating_ip_address, g.fixed_port_id from (SELECT e.*,f.name AS payer_account FROM (SELECT a.project_id,  a.floating_ip_address,  a.fixed_port_id, a.fixed_ip_address , b.payer_id FROM (SELECT neutron.floatingips.project_id, neutron.floatingips.floating_ip_address, neutron.floatingips.fixed_port_id, neutron.floatingips.fixed_ip_address FROM neutron.floatingips ) a LEFT JOIN (select keystone.assignment.actor_id as payer_id ,keystone.assignment.target_id  from keystone.assignment where keystone.assignment.role_id = "22f8b7794e5f40f4a21b1c74087936ae") b on a.project_id = b.target_id ) e left join (select keystone.local_user.user_id, keystone.local_user.name  FROM keystone.local_user) f on e.payer_id=f.user_id WHERE f.name REGEXP 'jcloud_' AND e.fixed_port_id IS not NULL  AND e.floating_ip_address REGEXP '111.' AND e.fixed_ip_address IS NOT NULL)g  left JOIN (select keystone.project.name AS project_name, keystone.project.id FROM keystone.project) h on g.project_id=h.id ) i LEFT JOIN (select neutron.ports.id, neutron.ports.device_id , neutron.ports.device_owner from neutron.ports  ) j ON i.fixed_port_id=j.id ) k LEFT JOIN (SELECT trove.clusters.name AS database_name, trove.clusters.id FROM trove.clusters WHERE trove.clusters.deleted = 0) m ON k.device_id = m.id WHERE k.device_owner REGEXP 'database'
+'''
+
+# 连接设置 连接数据部mysql 用户名user_CJ 密码Xiejing@2019 地址10.119.1.101：3306 database：testdb_CJ
 class DatabaseAccess():
 	#初始化属性
     def __init__(self):
@@ -91,18 +102,18 @@ class DatabaseAccess():
             self.__db.close()
         return (data,field_names)
 
-    def insertData(self,tablename, user_account,project_name,vm_name,vm_state,vcpus,memory_mb, timestamp):
-        sql = "insert into testdb_CJ.jcloud_%s values ('%s','%s','%s','%s',%d,%d,'%s')" % (tablename, user_account,project_name,vm_name,vm_state,vcpus,memory_mb,timestamp)
-        try:
-            self.isConnectionOpen()
-            with self.__db.cursor() as cursor:
-                cursor.execute(sql)
-                self.__db.commit()
-        except Exception as e:
-            logger.error("%s : %s" % (str(e), sql))
-            self.__db.rollback()
-        finally:
-            self.__db.close()
+    # def insertData(self,tablename, user_account,project_name,vm_name,vm_state,vcpus,memory_mb, timestamp):
+        # sql = "insert into testdb_CJ.jcloud_%s values ('%s','%s','%s','%s',%d,%d,'%s')" % (tablename, user_account,project_name,vm_name,vm_state,vcpus,memory_mb,timestamp)
+        # try:
+            # self.isConnectionOpen()
+            # with self.__db.cursor() as cursor:
+                # cursor.execute(sql)
+                # self.__db.commit()
+        # except Exception as e:
+            # logger.error("%s : %s" % (str(e), sql))
+            # self.__db.rollback()
+        # finally:
+            # self.__db.close()
     def genSql(self,tablename, row, current_time):
         sql = "insert into %s values (" % tablename
         for item in row:
@@ -110,7 +121,12 @@ class DatabaseAccess():
                 sql += "'" + item + "',"
             elif isinstance(item,int):
                 sql += str(item) + ","
+            elif isinstance(item,float):
+                sql += str(item) + ","
+            elif item is None:
+                sql += 'NULL' + ","
             else:
+                logger.error(",".join([str(i) for i in row]))
                 return ""
         sql += "'" + str(current_time) + "')"
         return sql
@@ -165,7 +181,6 @@ def RetrieveDataFromMysql(sql):
         data,field_names = [],[]
     finally:
         db.close()
-
     return (data,field_names)
 
 
@@ -188,26 +203,41 @@ def Merge(dbdata,outputfile):
 def insert_jcloud_mergetable(tablename,dataset):
     nowdate = datetime.now()
     today = str(nowdate.year) + "-" + str(nowdate.month) + "-" + str(nowdate.day) + " " + str(nowdate.hour) + ":00"
-    
     db=DatabaseAccess()
-    
-    sql = "select max(TIMESTAMP) from testdb_CJ.jcloud_%s" % tablename
-    if (datetime.strptime(today,'%Y-%m-%d %H:%M') != db.getData(sql)[0][0][0]):
-        db.insertDataSet(tablename, dataset,today)
+    # sql = "select max(TIMESTAMP) from testdb_CJ.jcloud_%s" % tablename
+    # if (datetime.strptime(today,'%Y-%m-%d %H:%M') != db.getData(sql)[0][0][0]):
+        # db.insertDataSet(tablename, dataset,today)
+    db.insertDataSet(tablename, dataset,today)
 
 if __name__ == "__main__":
-    db_data_1 = RetrieveDataFromMysql(sql_vm)
-    outputfile_1 = opt.outputfile + "_vm"
-    Merge(db_data_1,outputfile_1)
+    # db_data_1 = RetrieveDataFromMysql(sql_vm)
+    # outputfile_1 = opt.outputfile + "_vm"
+    # Merge(db_data_1,outputfile_1)
     
-    outputfile_2 = opt.outputfile + "_storage"
-    db_data_2 = RetrieveDataFromMysql(sql_storage)
-    Merge(db_data_2,outputfile_2)
+    # outputfile_2 = opt.outputfile + "_storage"
+    # db_data_2 = RetrieveDataFromMysql(sql_storage)
+    # Merge(db_data_2,outputfile_2)
     
     outputfile_3 = opt.outputfile + "_backup"
     db_data_3 = RetrieveDataFromMysql(sql_backup)
     Merge(db_data_3,outputfile_3)
     
-    outputfile_4 = opt.outputfile + "_snapshot"
-    db_data_4 = RetrieveDataFromMysql(sql_snapshot)
-    Merge(db_data_4,outputfile_4)
+    # outputfile_4 = opt.outputfile + "_snapshot"
+    # db_data_4 = RetrieveDataFromMysql(sql_snapshot)
+    # Merge(db_data_4,outputfile_4)
+    
+    # outputfile_5 = opt.outputfile + "_ip"
+    # db_data_5 = RetrieveDataFromMysql(sql_ip_null)
+    # Merge(db_data_5,outputfile_5)
+    
+    # outputfile_6 = opt.outputfile + "_ip"
+    # db_data_6 = RetrieveDataFromMysql(sql_ip_vm)
+    # Merge(db_data_6,outputfile_6)
+    
+    # outputfile_7 = opt.outputfile + "_ip"
+    # db_data_7 = RetrieveDataFromMysql(sql_ip_router)
+    # Merge(db_data_7,outputfile_7)
+    
+    # outputfile_8 = opt.outputfile + "_ip"
+    # db_data_8 = RetrieveDataFromMysql(sql_ip_database)
+    # Merge(db_data_8,outputfile_8)
